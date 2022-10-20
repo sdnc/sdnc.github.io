a6788e907c1e843216659a1077bf7e2bec10d5f2
sdnc <dza.sara@gmail.com>
2022-10-20 14:52:42 +1100

    fix typos

--- src/js/typed.js
+++ src/js/typed.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 var options = {
-  strings: ["I build websites", "I make cool programs", "I'm a full-stack developer and business analyst"],
+  strings: ["I build websites", "I make cool programs", "I'm a full stack developer and business analyst"],
   typeSpeed: 120,
   backSpeed: 50,
   backDelay: 1500
